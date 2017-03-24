f707057b762d1ae1b358c09a7f3f95138a5c4214
Morgan Williams <morgan.alysse@yahoo.com>
2017-03-23 23:44:26 -0400

    lex Function Definition

--- syntax-analysis.c
+++ syntax-analysis.c
@@ -72,6 +72,57 @@ void getNonBlank()
         getChar();
 }
 
+int lex()
+{
+    lexLen = 0;
+    getNonBlank();
+    
+    switch(charClass)
+    {
+        /* Parse Identifiers */
+        case LETTER:
+            addChar();
+            getChar();
+            while(charClass == LETTER || charClass == DIGIT)
+            {
+                addChar();
+                getChar();
+            }
+            nextToken = IDENT;
+            break;
+            
+        /* Parse Integer Literals */
+        case DIGIT:
+            addChar();
+            getChar();
+            while (charClass == DIGIT)
+            {
+                addChar();
+                getChar();
+            }
+            nextToken = INT_LIT;
+            break;
+            
+        /* Parentheses and Operators */
+        case UNKNOWN:
+            lookup(nextChar);
+            getChar();
+            break;
+            
+        /* EOF */
+        case EOF:
+            nextToken = EOF;
+            lexeme[0] = 'E';
+            lexeme[1] = 'O';
+            lexeme[2] = 'F';
+            lexeme[3] = 0;
+            break;
+    } /* End of Switch */
+    
+    printf("Next token is: %d, Next lexeme is %s\n", nextToken, lexeme);
+    return nextToken;
+}
+
 /* Main Driver */
 main () {
     
