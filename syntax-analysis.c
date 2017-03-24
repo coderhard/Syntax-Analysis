d771ff50afd832e31fff1b44c2f2d4899ee85813
Morgan Williams <morgan.alysse@yahoo.com>
2017-03-23 21:48:35 -0400

    Defining token codes.

--- syntax-analysis.c
+++ syntax-analysis.c
@@ -25,6 +25,15 @@ FILE * in_fp;
 #define UNKNOWN 99
 
 /* Token Codes */
+#define INT_LIT 10
+#define IDENT 11
+#define ASSIGN_OP 20
+#define ADD_OP 21
+#define SUB_OP 22
+#define MULT_OP 23
+#define DIV_OP 24
+#define LEFT_PAREN 25
+#define RIGHT_PAREN 26
 
 /* Function Definitions */
 
