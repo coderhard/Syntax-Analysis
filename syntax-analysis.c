7886338f21af1edf00a47007ff99e08f0d3e3fe3
Morgan Williams <morgan.alysse@yahoo.com>
2017-03-23 20:36:19 -0400

    Creating the file.

---
+++ syntax-analysis.c
@@ -0,0 +1,23 @@
+/* Syntax Analysis Programming Assignment */
+/* syntax-analysis.c */
+/* Morgan Williams */
+/* CSCI 350 Structures of Programming Languages */ 
+
+#include <stdio.h> 
+#include <ctype.h>
+
+/* Global Declarations */
+/* Variables */
+
+/* Function Declarations */
+
+/* Character Classes */
+
+/* Token Codes */
+
+/* Function Definitions */
+
+/* Main Driver */
+main () {
+    
+}
