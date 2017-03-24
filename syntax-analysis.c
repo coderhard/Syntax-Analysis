9d285125d89f276ba997b366dad1437da9a8074e
Morgan Williams <morgan.alysse@yahoo.com>
2017-03-23 21:39:41 -0400

    Defining variables.

--- syntax-analysis.c
+++ syntax-analysis.c
@@ -1,13 +1,21 @@
 /* Syntax Analysis Programming Assignment */
 /* syntax-analysis.c */
 /* Morgan Williams */
-/* CSCI 350 Structures of Programming Languages */ 
+/* CSCI 350 Structures of Programming Languages */
 
 #include <stdio.h> 
 #include <ctype.h>
 
 /* Global Declarations */
 /* Variables */
+int charClass;
+char lexeme [100];
+char nextChar;
+int lexLen;
+int token;
+int nextToken;
+
+FILE * in_fp;
 
 /* Function Declarations */
 
